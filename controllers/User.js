fcf78a69757cc66a1018c6620a01438ac843d7d1
sony-nurdianto <sonynurdianto445@gmail.com>
2020-04-29 19:54:50 +0700

    oke perbaikan salah paham soal done

--- controllers/User.js
+++ controllers/User.js
@@ -10,11 +10,22 @@ module.exports = {
           name : req.body.name,
           phone : req.body.phone,
           password: req.body.password,
+          confirmPassword: req.body.confirmPassword
         })
-          .then((response) => res.json(response))
-          .catch((err) => {
-            throw err;
-          });
+        .then((response) => {
+          if(req.body.password !== req.body.confirmPassword){
+            res.send("your confirm pasword is not match with your password please try again")
+          }else(
+            res.json(response)
+          )
+        })    
+          .catch ((err) => {
+            throw err
+          })
+          // .then((response) => res.json(response))
+          // .catch((err) => {
+          //   throw err;
+          // });
       },
     
       authenticated: function (req, res, next) {
--- models/User.js
+++ models/User.js
@@ -20,6 +20,10 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  confirmPassword:{
+    type : String,
+    required : true,
+  }
 });
 
 userSchema.pre("save", function (next) {
